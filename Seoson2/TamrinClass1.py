8d0631e6257027bf1bedce46aa04caf69a78bc2d
unknown <alirezaj1381510@gmail.com>
2023-11-06 18:32:36 +0330

    Tamrin class

--- Seoson2/TamrinClass1.py
+++ Seoson2/TamrinClass1.py
@@ -1,7 +1,15 @@
-class person :
+class Person:
+    count=0
     def __init__(self,name,family):
         self.__name=name
         self.__family=family
-      
-        
+        Person.count+=1
+    def __str__(self):
+        return self.__name+"\t\t"+self.__family  
+
+
+person1=Person("alireza","jahangard")
+print(str(person1))
+print(Person.count)
+
         
